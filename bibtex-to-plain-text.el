a9f7a5a6908b8da9f8a97b848980bf918db3926f
Nick Van Horn <nemo1211@gmail.com>
2012-09-26 15:52:45 -0400

    Additional support for entry type "InProceedings" added (testing is still needed)

--- bibtex-to-plain-text.el
+++ bibtex-to-plain-text.el
@@ -20,11 +20,131 @@
 ;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 ;; Boston, MA 02111-1307, USA.
 
+;; bibtex-to-plain-text.el
+;; =======================
 
+;; Tools for quickly creating plain text bibliographic references from
+;; BibTeX entries
+
+;; The initial purpose of this package was to enable easy conversion from
+;; BibTeX/LaTeX citations into a plain text format that can be easily
+;; pasted into other programs that are unfriendly to LaTeX (or for
+;; quickly sharing references with colleagues or friends through email,
+;; etc.).
+
+;; Currently, this package allows you to easily convert either BibTeX
+;; formatted text, or latex \cite{} commands in a buffer into plain text
+;; APA formatted references. Of course, formatting such as underlining
+;; and italics will be missing from the reference string. As of now, if
+;; you plan to paste the formatted string into another program (such as
+;; M$ Word), you will need to manually add such markup. Future versions
+;; will hopefully remove this necessity.
+
+;; bibtex-to-plain-text is agnostic to the referencing style used as
+;; output. The default is APA styling. Styling behavior is controlled by
+;; the variable bibtex-to-plain-text-style, which is set by
+;; default to an association list called
+;; bibtex-to-plain-text-apa-formats that contains the
+;; formatting styles for each BibTeX entry type (article, book,
+;; incollection, etc.). Additional styles (MLA, Chicago style, etc.) will
+;; be added in time. Users can also provide their own custom style rules
+;; by creating custom association lists according to those defined in
+;; bibtex-to-plain-text.el. By setting
+;; bibtex-to-plain-text-style to the custom list, the output
+;; behavior of bibtex-to-plain-text can be modified:
+
+;;  (setq bibtex-to-plain-text-style
+;; bibtex-to-plain-text-my-custom-format-list) 
+
+;; Setup
+;; =====
+
+;; This package requires that bibtex and
+;; reftex-cite are installed.
+
+;; Provided bibtex-to-plain-text.el is on your load path, a simple
+;; require will get you up and running.
+
+;;  (require 'bibtex-to-plain-text) 
+
+;; To use the latex- commands below, you will need to ensure
+;; that your LaTeX/BibTeX installations are set up properly. Most
+;; importantly, your .bib file(s) specified in your LaTeX
+;; \bibliography{} statements should be on the correct
+;; path. This is not a requirement for the bibtex- commands.
+
+;; Usage
+;; =====
+
+;; The package makes several functions available. <i>Expect these
+;; functions to be renamed soon.</i>
+
+;; bibtex-create-plain-text-reference
+;; ----------------------------------
+
+;; This converts a BibTeX entry under point (selecting a region is NOT
+;; required) to a plain text reference. The result is pushed to the kill
+;; ring. Example:
+
+;; @article{petrov2011,
+;;   title =	 {Dissociable perceptual-learning mechanisms revealed
+;;                   by diffusion-model analysis},
+;;   author =	 {Petrov, A.A. and Van Horn, N.M. and Ratcliff, R.},
+;;   journal =	 {Psychonomic bulletin \& review},
+;;   volume =	 18,
+;;   number =	 3,
+;;   pages =	 {490--497},
+;;   year =	 2011,
+;;   publisher = {Springer}
+;; } 
+
+;; Running M-x bibtex-create-plain-text-reference with point
+;; anywhere inside the above BibTeX entry will produce the following APA
+;; reference:
+
+;;  Petrov, A.A. and Van Horn, N.M. and Ratcliff,
+;; R. (2011). Dissociable perceptual-learning mechanisms revealed by
+;; diffusion-model analysis. Psychonomic Bulletin & Review, 18(3),
+;; 490-497.  
+
+;; bibtex-convert-buffer-to-plain-text
+;; -----------------------------------
+
+;; This is a wrapper for bibtex-create-plain-text-reference
+;; described above. The current buffer is searched and all BibTeX entries
+;; are converted to plain text. The results are written to a buffer named
+;; \*references\*. The contents of the current buffer can contain a
+;; mixture of BibTeX markup and other text. Thus,
+;; bibtex-convert-buffer-to-plain-text will scrape your
+;; buffer of any BibTeX entries and convert them into a references list.
+
+;; latex-convert-buffer-to-plain-text
+;; ----------------------------------
+
+;; This function should be run inside of a LaTeX document containing one
+;; or more \cite{} commands. Provided you have specified a
+;; bibliography file in \bibliography{} statement, the
+;; function will create a plain text reference list formatted according
+;; to the standards associated with the reference type of each citation
+;; (e.g., article, book, etc. will be treated uniquely). This should work
+;; across multi-file LaTeX documents as well (i.e., those using
+;; \input{} statements).
+
+;; latex-create-plain-text-reference
+;; ---------------------------------
+
+;; <b>Currently not working properly!!!</b>
+
+;; This function creates a plain text formatted reference of the
+;; \cite{} entry under point. The reference is pushed to the
+;; kill ring.
+
+;; ##################################################################
+;; 
 ;; The variable bibtex-to-plain-text-style controls the behavior of
 ;; the interactive function `bibtex-create-plain-text-reference'. The
 ;; way this works is: Each entry type will be represented by an
-;; association list. The keyin the alist is the entry type, and the
+;; association list. The key in the alist is the entry type, and the
 ;; value is a pair of lists. The first item in the pair will consist
 ;; of a list of the BibTex fields required to format the entry
 ;; type. The second item will be a formatting string showing how to
@@ -70,11 +190,16 @@
 (setq bibtex-to-plain-text-apa-formats
       '(
 	("article" . (("author" "year" "title" "journal" "volume"
-			"number" "pages") 
-		       "%s (%s). %s. %s, %s|(%s)|, %s."))
+		       "number" "pages") 
+		      "%s (%s). %s. %s, %s|(%s)|, %s."))
 	("book" . (("author" "year" "title" "editor" "address"
-		     "publisher") 
-		    "%s (%s). %s.| %s| |%s: |%s."))))
+		    "publisher") 
+		   "%s (%s). %s.| %s| |%s: |%s."))
+	;; inproceedings can have either "journal" or "booktitle"
+	("inproceedings" . (("author" "year" "title" "editor"
+			     "booktitle" "journal" "pages" "address"
+			     "publisher")
+	    "%s (%s). %s.| In |%s| (Ed.),| %s%s| (pp. |%s|)|.| %s:| %s"))))
 
 ;; This is what the function bibtex-create-plain-text-reference
 ;; actually uses. The user can point this to any properly structured
@@ -141,7 +266,7 @@ bibtex-to-plain-text-style"
       (setq currData (bibtex-text-in-field x))
       ;; We may need to perform some pre-processing on the text
       (cond
-       ((equal "pages" x)
+       ((and (equal "pages" x) (equal "" currData))
 	(progn
 	  (setq currData (when (string-match "--" currData)
 			   (replace-match "-" nil nil currData)))))
